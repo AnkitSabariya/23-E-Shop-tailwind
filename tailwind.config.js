e00db85af7003f0de885009f4b33f7e486882db3
Mr Ankit <ankitsabariya@gmail.com>
2024-12-07 10:29:52 +0530

    23

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html', // Path to your HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class', // This enables class-based dark mode
+}
